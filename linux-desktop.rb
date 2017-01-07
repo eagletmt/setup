8e5b8dfab0f1d00568979469a66b19a00f5cde6d
Kohei Suzuki <eagletmt@gmail.com>
2017-01-07 14:30:09 +0900

    Add remmina

--- linux-desktop.rb
+++ linux-desktop.rb
@@ -3,13 +3,18 @@ node[:home] = "/home/#{node[:user]}"
 
 include_recipe 'cookbooks/functions'
 
+# Basic non-GUI apps
 include_cookbook 'tmux'
 include_cookbook 'zsh'
 include_cookbook 'vim'
 include_cookbook 'uim'
 include_cookbook 'yaskkserv'
 
+# Basic GUI apps
 include_cookbook 'urxvt'
 include_cookbook 'xmonad'
 include_cookbook 'firefox'
 include_cookbook 'envchain'
+
+# Extra apps
+include_cookbook 'remmina'
