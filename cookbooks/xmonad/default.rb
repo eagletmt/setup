08c5460126360eb32ef8e2df19a8ef4d29499bd5
Kohei Suzuki <eagletmt@gmail.com>
2017-01-07 14:19:48 +0900

    Install xmobar from pacman

--- cookbooks/xmonad/default.rb
+++ cookbooks/xmonad/default.rb
@@ -1,11 +1,9 @@
 include_cookbook 'ghc'
+package 'xmobar'
 
 cabal 'xmonad'
 cabal 'xmonad-contrib' do
   options %w[-fuse_xft]
-end
-cabal 'xmobar' do
-  options %w[-fwith_xft]
 end
 
 include_cookbook 'dotfiles'
