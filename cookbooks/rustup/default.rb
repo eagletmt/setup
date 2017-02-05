0c82b9742ea63212f19b34154df5423b10737118
Kohei Suzuki <eagletmt@gmail.com>
2017-02-05 16:13:06 +0900

    Skip grep

--- cookbooks/rustup/default.rb
+++ cookbooks/rustup/default.rb
@@ -6,7 +6,10 @@ define :rustup_toolchain do
   execute "rustup toolchain add #{toolchain}" do
     user node[:user]
     command ['rustup', 'toolchain', 'add', toolchain]
-    not_if "rustup toolchain list | grep -q '^#{toolchain}-'"
+    not_if do
+      out, _, status = Open3.capture3('rustup', 'toolchain', 'list')
+      status.success? && out.lines.any? { |line| line.start_with?("#{toolchain}-") }
+    end
   end
 end
 
