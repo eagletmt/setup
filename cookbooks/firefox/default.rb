b24e5da82a592e8796a2e31a7e2747373dcb28f3
Kohei Suzuki <eagletmt@gmail.com>
2017-02-18 02:48:45 +0900

    :wave: Vimperator

--- cookbooks/firefox/default.rb
+++ cookbooks/firefox/default.rb
@@ -1,12 +1,6 @@
 package 'firefox'
 
 include_cookbook 'dotfiles'
-%w[vimperatorrc vimperator].each do |file|
-  link "#{node[:home]}/.#{file}" do
-    to "#{node[:dotfiles][:dir]}/dot.#{file}"
-  end
-end
-ghq 'eagletmt/misc'
 
 link "#{node[:home]}/.config/vimfx" do
   to "#{node[:dotfiles][:dir]}/dot.config/vimfx"
