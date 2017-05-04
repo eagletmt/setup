6dd68a24d86176e367d4bd993db8cf6e3088657a
Kohei Suzuki <eagletmt@gmail.com>
2017-05-04 21:41:46 +0900

    Limit journald size

--- cookbooks/systemd/default.rb
+++ cookbooks/systemd/default.rb
@@ -3,3 +3,14 @@ remote_file '/etc/systemd/system.conf' do
   group 'root'
   mode '644'
 end
+
+directory '/etc/systemd/journald.conf.d' do
+  owner 'root'
+  group 'root'
+  mode '755'
+end
+remote_file '/etc/systemd/journald.conf.d/limit.conf' do
+  owner 'root'
+  group 'root'
+  mode '644'
+end
