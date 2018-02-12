27562d973503d44714b42b1d55bd3f32cab32481
Kohei Suzuki <eagletmt@gmail.com>
2018-02-12 16:56:00 +0900

    Create parent directories

--- cookbooks/ssh-agent/default.rb
+++ cookbooks/ssh-agent/default.rb
@@ -1,6 +1,17 @@
 include_cookbook 'openssh'
 include_cookbook 'systemd-user'
 
+[
+  "#{node[:home]}/.config",
+  "#{node[:home]}/.config/systemd",
+  "#{node[:home]}/.config/systemd/user",
+].each do |d|
+  directory d do
+    owner node[:user]
+    group node[:group]
+    mode '755'
+  end
+end
 remote_file "#{node[:home]}/.config/systemd/user/ssh-agent.service" do
   owner node[:user]
   group node[:group]
